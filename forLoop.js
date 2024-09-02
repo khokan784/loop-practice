44fa9016f5b64f227d01c95df40af2c9fec0d02e
khokan <khokancrmt@gmail.com>
2024-09-02 13:37:52 +0600

    added file

---
+++ forLoop.js
@@ -0,0 +1,25 @@
+// for(loop variable declaration; loop condition; loop variable change ){
+// loop statement}
+
+
+// for(let num = 0; num <= 5; num++){
+//     console.log(num);
+// }
+
+
+//even hisab
+
+// for(let i = 0; i <= 20; i = i + 2) {
+//     console.log(i);
+// }
+
+
+//odd hisab
+
+// for(let i = 1; i <= 20; i=i+2) {
+//     console.log(i);
+// }
+
+for(let i = 1; i <= 5; i++) {
+    console.log('Hello');
+}
---
+++ loop.js
@@ -0,0 +1,17 @@
+// for loop
+
+// const numbers = [12, 24, 17, 20,25];
+// for(const num of numbers) {
+//     console.log(num);
+// }
+
+// while loop
+
+let num = 1;
+while(num <= 10) {
+    console.log('looping', num);
+    // num = num + 1;
+    // num += 1;
+    num++;
+}
+
